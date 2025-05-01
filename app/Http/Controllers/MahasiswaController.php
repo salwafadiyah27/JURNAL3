25cda7e32b1cd907b33e8d086ba4102e4268cbce
salwafadiyah27 <salwafadiyah27@gmail.com>
2025-05-01 10:51:25 +0700

    Initial commit

---
+++ app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MahasiswaController extends Controller
+{
+    public function index()
+    {
+        // ==================2==================
+        // - Buat object mahasiswa dengan data dummy (nama, nim, email, jurusan, fakultas, foto)
+        $mahasiswa = (object) [
+            'nama' => 'Budi Santoso',
+            'nim' => '123456789',
+            'email' => 'budi@examaple.com',
+            'jurusan' => 'S1 Sistem Informasi',
+            'fakultas' => 'Fakultas Rekayasa Industri',
+            'foto' => 'images/profil.png' // path relatif dari public/
+        ];
+
+        // - Kirim object tersebut ke view 'profil'
+        return view('profil', compact('mahasiswa'));
+    }
+}
